bb7255049b8cf4f1d396a24e7ec4a2aadf1edba9
dherediat <dherediat97@gmail.com>
2024-11-21 16:46:38 +0100

    feature: initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import styles from './footer.module.css';
+
+import PokemonPic from '../assets/pikachu.png';
+import LocationPic from '../assets/pointer.png';
+import ItemsPic from '../assets/pokeball.png';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Link
+        onClick={(event) => event.preventDefault()}
+        className={styles.footerLink}
+        to="/pokemons"
+      >
+        <img className={styles.footerIcon} src={PokemonPic} alt="Pokemons" />
+        Pokemons
+      </Link>
+      <Link
+        onClick={(event) => event.preventDefault()}
+        className={styles.footerLink}
+        to="/items"
+      >
+        <img className={styles.footerIcon} src={ItemsPic} alt="Items" />
+        Items
+      </Link>
+      <Link
+        onClick={(event) => event.preventDefault()}
+        className={styles.footerLink}
+        to="/locations"
+      >
+        <img className={styles.footerIcon} src={LocationPic} alt="Locations" />
+        Locations
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
